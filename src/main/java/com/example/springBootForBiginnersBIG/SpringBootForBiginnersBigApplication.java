9a46bf0378f74430fab2e4ab6ab251a1caeb5e50
zaigamarra <zaigamarra@gmail.com>
2021-05-28 22:22:28 -0300

    Uso de Spring Boot

---
+++ src/main/java/com/example/springBootForBiginnersBIG/SpringBootForBiginnersBigApplication.java
@@ -0,0 +1,15 @@
+package com.example.springBootForBiginnersBIG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootForBiginnersBigApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootForBiginnersBigApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/springBootForBiginnersBIG/SpringBootForBiginnersBigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootForBiginnersBIG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootForBiginnersBigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
